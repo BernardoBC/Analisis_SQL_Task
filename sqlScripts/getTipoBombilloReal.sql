c21480e1238c8817e59cc53f82a6904443a0791a
BernardoBC <bernardobonillac@gmail.com>
2016-02-12 13:30:39 -0600

    getBombilloReal

--- sqlScripts/getTipoBombilloReal.sql
+++ sqlScripts/getTipoBombilloReal.sql
@@ -1,3 +1,6 @@
-SELECT        @bombillo = TipoBombillo
+DECLARE @resultado int; 
+SELECT        @resultado = TipoBombillo
 FROM            PreguntaUno
 WHERE        (VoltajeMinimo <= @voltaje) AND (VoltajeMaximo >= @voltaje)
+
+CASE WHEN (@resultado IS NULL) THEN @error = @resultado ELSE @bombillo = @resultado END
